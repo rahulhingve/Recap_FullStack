54fee3c1773897e93236e444d989a4e135373728
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-12-23 12:00:27 +0530

    object playground

---
+++ JavaScript/Basic/object.js
@@ -0,0 +1,23 @@
+
+//Objects in js  old and new methods
+
+var user = {
+    id:1,
+    name:"rahul",
+    age  :"sweet 17",
+    ageNumber :17,
+    height :6.2,
+    
+}
+// calling data with console.log
+// console.log("id is  : "+user.id);
+// console.log("name is : "+user.name);
+// console.log("age is : "+user.age);
+// console.log("age in number : "+user.ageNumber);
+// console.log("height is : "+user.height);
+
+const alldata  = JSON.stringify(user)
+
+
+console.log(alldata);
+console.log(user);
