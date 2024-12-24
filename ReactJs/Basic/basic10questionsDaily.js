578551415f7bbacac47417cf517e4735fb9181b0
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-12-24 13:19:49 +0530

    todays 10 react ques

---
+++ ReactJs/Basic/basic10questionsDaily.js
@@ -0,0 +1,11 @@
+// Most Asked ReactJS Interview Questions
+// What is ReactJS?
+// Why ReactJS is Used?
+// How Does ReactJS work?
+// What are the features of ReactJS?
+// What is JSX?
+// How to create components in ReactJS?
+// What are the advantages of ReactJS?
+// Differentiate between real DOM and virtual DOM?
+// What are forms in ReactJS?
+// How is React different from React Native?a
