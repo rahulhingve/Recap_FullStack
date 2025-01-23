ac3f771c2c83942595442f32ddc281fd18a11c7c
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-01-23 22:48:02 +0530

    for loop revice

---
+++ C++/basics/forLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Hello Rahul this is " << i << "th iteration of the loop" << endl;
+
+    }
+    return 0;
+}
