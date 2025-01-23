9971509c56751e5454940a0312b4a701a6a1b986
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-01-23 23:00:50 +0530

    nested For Loop

--- C++/basics/forLoop.cpp
+++ C++/basics/forLoop.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 5; i++)
+    for (int i = 1; i <= 10; i++)
     {
         cout << "Hello Rahul this is " << i << "th iteration of the loop" << endl;
 
